20d41f2a0d6d747d96fde1183d6d3e22fab279b3
EduarRugamas <juaneduardo021299@hotmail.com>
2022-05-27 10:50:01 -0600

    add style custom text

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
+const searchClient = algoliasearch('VFM4X0N23A', 'd16d4804f78c38777368ea8ced79e56e');
+
 const search = instantsearch({
-    indexName: 'janesRoots',
-    searchClient: algoliasearch('VFM4X0N23A', 'd16d4804f78c38777368ea8ced79e56e')
+    indexName: 'menu-products-demo',
+    searchClient
 });
 
 search.addWidgets([
+
+    instantsearch.widgets.searchBox({
+        container: '#searchbox'
+    }),
+
     instantsearch.widgets.hits({
-        container: '#conta',
+        container: '#hits',
         templates: {
-            value: function (suggestion) {
-                console.log(suggestion.name)
-            }
+            item: `
+                <p class="texto"> {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</p>
+              `
         }
     })
 ]);
