d292a111b081ee9f3c6eab1508095af69bac2807
EduarRugamas <juaneduardo021299@hotmail.com>
2022-05-28 23:17:52 -0600

    possible solution 7

--- js/index.js
+++ js/index.js
@@ -59,7 +59,7 @@ search.addWidgets([
     }),
     instantsearch.widgets.refinementList({
         container: '#numericMenu-available-weights',
-        attribute: 'activities',
+        attribute: 'root_types',
     }),
 
     instantsearch.widgets.hits({
