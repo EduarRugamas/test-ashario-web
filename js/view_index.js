9b254c0a90793b16341518cd716a1184fff030f2
EduarRugamas <juaneduardo021299@hotmail.com>
2022-05-31 13:25:26 -0600

    resolving bug 5

--- js/view_index.js
+++ js/view_index.js
@@ -9,10 +9,10 @@ search.addWidgets([
 
     instantsearch.widgets.searchBox({
         container: '#searchBox',
-        // template: {
-        //     item: `<input type="text" class="form-control w-100" placeholder="Search for Products">
-        //            <span class="input-group-text cursor-pointer bg-transparent"><i class='bx bx-search'></i></span>`
-        // }
+        template: {
+            item: `<input type="text" class="form-control w-100" placeholder="Search for Products">
+                   <span class="input-group-text cursor-pointer bg-transparent"><i class='bx bx-search'></i></span>`
+        }
     }),
 ]);
 
