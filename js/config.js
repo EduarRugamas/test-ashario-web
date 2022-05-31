5ac955c6bfe1443440ed6c06453478b3aa22d0bc
EduarRugamas <juaneduardo021299@hotmail.com>
2022-05-31 12:57:55 -0600

    add new function

--- js/config.js
+++ js/config.js
@@ -1,2 +1,9 @@
-export const searchClient = algoliasearch('VFM4X0N23A', 'd16d4804f78c38777368ea8ced79e56e');
+const searchClient = algoliasearch('VFM4X0N23A', 'd16d4804f78c38777368ea8ced79e56e');
+
+export function callbackInstantSearch(value) {
+    return instantsearch({
+        indexName: `${value}`,
+        searchClient
+    });
+}
 
