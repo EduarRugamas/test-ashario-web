0ef526f58bc053e2a73ebd4b5b3e4ef71c0c6daa
EduarRugamas <juaneduardo021299@hotmail.com>
2022-05-31 13:16:13 -0600

    add searcBox custom

--- js/view_index.js
+++ js/view_index.js
@@ -1,3 +1,20 @@
-import { searchClient } from './config';
+import {searchClient} from './config';
+
+const search = instantsearch({
+    indexName: 'products-demo',
+    searchClient
+});
+
+search.addWidgets([
+    instantsearch.addWidgets({
+        container: '#searchBox',
+        template: {
+            item: `<input type="text" class="form-control w-100" placeholder="Search for Products">
+                   <span class="input-group-text cursor-pointer bg-transparent"><i class='bx bx-search'></i></span>`
+        }
+    }),
+]);
+
+search.start();
 
 
