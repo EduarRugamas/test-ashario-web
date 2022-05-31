7718d03c4bb9945e0b0cd3060d3a97ffc2509f4a
EduarRugamas <juaneduardo021299@hotmail.com>
2022-05-31 13:30:45 -0600

    add resolving 7

--- js/view_index.js
+++ js/view_index.js
@@ -9,10 +9,7 @@ search.addWidgets([
 
     instantsearch.widgets.searchBox({
         container: '#searchBox',
-        template: {
-            items: `<input type="text" class="form-control w-100" placeholder="Search for Products">
-                   <span class="input-group-text cursor-pointer bg-transparent"><i class='bx bx-search'></i></span>`
-        }
+
     }),
 ]);
 
