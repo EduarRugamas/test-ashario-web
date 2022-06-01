69fad8906fd9c3ed62fe9248a6dbeab798685f80
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-01 11:19:47 -0600

    add new card

--- js/shop-grid-left-sidebar.js
+++ js/shop-grid-left-sidebar.js
@@ -15,47 +15,47 @@ search.addWidgets([
         }
     }),
 
-    instantsearch.widgets.hits({
-        container: '#container-hits',
-        templates: {
-            item:
-            `
-            <div class="col">
-                                           <div class="card rounded-0 product-card">
-                                               
-                                                <img src="{{image_urls}}" class="card-img-top"
-                                                     alt="...">
-                                                <div class="card-body">
-                                                    <div class="product-info">
-                                                        <a href="javascript:;">
-                                                            <p class="product-catergory font-13 mb-1">{{brand}}</p>
-                                                            <p class="product-catergory font-13 mb-1">{{brand_subtype}}</p>
-                                                        </a>
-                                                        <a href="javascript:;">
-                                                            <h6 class="product-name mb-2">{{#helpers.highlight}}{"attribute": "name"}{{/helpers.highlight}}</h6>
-                                                        </a>
-                                                        <div class="d-flex align-items-center">
-                                                            <div class="mb-1 product-price"> 
-                                                                <span class="fs-5">\$ {{bucket_price}}</span>
-                                                            </div>
-                                                        </div>
-                                                        <div class="product-action mt-2">
-                                                            <div class="d-grid gap-2">
-                                                                <a href="javascript:;" class="btn btn-dark btn-ecomm">
-                                                                    <i class="bx bxs-cart-add"></i>Add to Cart</a>
-                                                                <a href="javascript:;" class="btn btn-light btn-ecomm"
-                                                                   data-bs-toggle="modal"
-                                                                   data-bs-target="#QuickViewProduct"><i
-                                                                        class="bx bx-zoom-in"></i>Quick View</a>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>  
-            </div>
-            `
-        }
-    }),
+    // instantsearch.widgets.hits({
+    //     container: '#container-hits',
+    //     templates: {
+    //         item:
+    //         `
+    //         <div class="col">
+    //                                        <div class="card rounded-0 product-card">
+    //
+    //                                             <img src="{{image_urls}}" class="card-img-top"
+    //                                                  alt="...">
+    //                                             <div class="card-body">
+    //                                                 <div class="product-info">
+    //                                                     <a href="javascript:;">
+    //                                                         <p class="product-catergory font-13 mb-1">{{brand}}</p>
+    //                                                         <p class="product-catergory font-13 mb-1">{{brand_subtype}}</p>
+    //                                                     </a>
+    //                                                     <a href="javascript:;">
+    //                                                         <h6 class="product-name mb-2">{{#helpers.highlight}}{"attribute": "name"}{{/helpers.highlight}}</h6>
+    //                                                     </a>
+    //                                                     <div class="d-flex align-items-center">
+    //                                                         <div class="mb-1 product-price">
+    //                                                             <span class="fs-5">\$ {{bucket_price}}</span>
+    //                                                         </div>
+    //                                                     </div>
+    //                                                     <div class="product-action mt-2">
+    //                                                         <div class="d-grid gap-2">
+    //                                                             <a href="javascript:;" class="btn btn-dark btn-ecomm">
+    //                                                                 <i class="bx bxs-cart-add"></i>Add to Cart</a>
+    //                                                             <a href="javascript:;" class="btn btn-light btn-ecomm"
+    //                                                                data-bs-toggle="modal"
+    //                                                                data-bs-target="#QuickViewProduct"><i
+    //                                                                     class="bx bx-zoom-in"></i>Quick View</a>
+    //                                                         </div>
+    //                                                     </div>
+    //                                                 </div>
+    //                                             </div>
+    //                                         </div>
+    //         </div>
+    //         `
+    //     }
+    // }),
 
     instantsearch.widgets.pagination({
         container: '#pagination-container',
