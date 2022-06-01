db3d68b221344512ab77a0b5540910da52390e4d
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-01 16:49:59 -0600

    agreganbdo nuevo codigo de las card view

--- js/shop-grid-left-sidebar.js
+++ js/shop-grid-left-sidebar.js
@@ -90,18 +90,21 @@ search.addWidgets([
        container: '#container-hits',
        templates: {
            item: `
-           <div class="tarjetas">
-             
-                 <img src="{{image_urls}}" alt="">
-             
-              <div class="content">
-                   <h2 class="string-name-product">{{#helpers.highlight}}{"attribute": "name"}{{/helpers.highlight}}</h2>
-                   <h4 class="string-brand-product">{{brand}}</h4>
-                   <h4 class="string-brand-sub-type-product">{{brand_subtype}}</h4>
-                   <p class="string-description-product">{{description}}</p>
-                   <a href="#">Quick View</a>
-              </div>
-           </div>
+            <div class="card-views">
+                <figure>
+                    <img src="{{image_urls}}" width="100" height="210">
+                </figure>
+                 <div class="contenido">
+                    <h3 class="text-brand">{{brand}}</h3>
+                    <h3 class="text-bran-sub-type">{{brand_subtype}}</h3>
+                    <div>
+                         <h6>{{#helpers.highlight}}{"attribute": "name"}{{/helpers.highlight}}</h6>
+                    </div>
+                   <p class="description">{{description}}</p>
+                   <span class="text-bucket_price">\\$ {{bucket_price}}</span>
+               </div>
+             </div>
+    
            `
        }
     }),
@@ -109,8 +112,8 @@ search.addWidgets([
     instantsearch.widgets.pagination({
         container: '#pagination-container',
         templates: {
-            first: `<a class="page-link" href="javascript:;" aria-label="Prev"> <i class='bx bx-chevron-left'></i> Prev</a>`,
-            next: `<a class="page-link" href="javascript:;" aria-label="Next"><i class='bx bx-chevron-right'></i>  Next </a>`
+            first: ` <i class='bx bx-chevron-left'></i>`,
+            next: `<i class='bx bx-chevron-right'></i>`
         }
     })
 
