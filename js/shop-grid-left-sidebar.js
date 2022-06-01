c86cfe6e027c9d52e16abc633c91d1f006681ffe
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-01 17:42:02 -0600

    add div container de card view

--- js/shop-grid-left-sidebar.js
+++ js/shop-grid-left-sidebar.js
@@ -95,13 +95,17 @@ search.addWidgets([
                     <figure>
                          <img src="{{image_urls}}" width="100" height="210">
                     </figure>
-<!--                <img src="{{image_urls}}" alt="{{name}}" />-->
+                    
                   <div class="hit-name">
                     {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
                   </div>
-                  <div class="hit-description">
-                    {{description}}
+                  
+                  <div class="contenido"> 
+                        <p class="description">{{description}}</p>
                   </div>
+<!--                  <div class="hit-description">-->
+<!--                    {{description}}-->
+<!--                  </div>-->
                   <div class="hit-price">\$ {{bucket_price}}</div>
         </div>
     
