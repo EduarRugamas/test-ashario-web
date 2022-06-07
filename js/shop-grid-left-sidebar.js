b7b2f47da00d253d5e16dd1cadaf627d7272dd3c
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-06 22:08:48 -0600

    add new style custom hits

--- js/shop-grid-left-sidebar.js
+++ js/shop-grid-left-sidebar.js
@@ -9,31 +9,52 @@ const search = instantsearch({
 const HitsRender = (renderOptions, isFirstRender) => {
     const { hits, results, sendEvent, widgetParams } = renderOptions;
     document.querySelector('#container-hits').innerHTML = `
-    
-        
-        
             ${hits.map( item =>
         `
-        <div class="product-info">
-            <a href="">
-                <p class="product-catergory font-13 mb-1">Catergory Name</p>
-            </a>
-            <a href="">
-                <h6 class="product-name mb-2">${item.name}</h6>
-            </a>
-            <div class="d-flex align-items-center">
-                <div class="mb-1 product-price">    
-                    <span class="me-1 text-decoration-line-through">$99.00</span>
-                    <span class="fs-5">$49.00</span>
+        <div class="col">
+            <div class="card rounded-0 product-card">
+                <div class="card-header bg-transparent border-bottom-0">
+                    <div class="d-flex align-items-center justify-content-end gap-3">
+                        <a href="javascript:;">
+                            <div class="product-compare"><span><i class="bx bx-git-compare"></i> Compare</span></div>
+                        </a>
+                        <a href="javascript:;">
+                        <div class="product-wishlist"> <i class="bx bx-heart"></i></div>
+                        </a>
+                    </div>
+                </div>
+                <img src="assets/images/products/01.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="product-info">
+                        <a href="javascript:;">
+                            <p class="product-catergory font-13 mb-1">Catergory Name</p>
+                        </a>
+                        <a href="javascript:;">
+                            <h6 class="product-name mb-2">Product Short Name</h6>
+                        </a>
+                        <div class="d-flex align-items-center">
+                            <div class="mb-1 product-price">\t<span class="me-1 text-decoration-line-through">$99.00</span>
+                                <span class="fs-5">$49.00</span>
+                            </div>
+                            <div class="cursor-pointer ms-auto">
+                                <i class="bx bxs-star text-warning"></i>
+                                <i class="bx bxs-star text-warning"></i>
+                                <i class="bx bxs-star text-warning"></i>
+                                <i class="bx bxs-star text-warning"></i>
+                                <i class="bx bxs-star text-warning"></i>
+                            </div>
+                        </div>
+                        <div class="product-action mt-2">
+                            <div class="d-grid gap-2">
+                                <a href="javascript:;" class="btn btn-dark btn-ecomm"><i class="bx bxs-cart-add"></i>Add to Cart</a>
+                                <a href="javascript:;" class="btn btn-light btn-ecomm" data-bs-toggle="modal" data-bs-target="#QuickViewProduct"><i class="bx bx-zoom-in"></i>Quick View</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="product-action mt-2">
-                <div class="d-grid gap-2">
-                    <a href="" class="btn btn-dark btn-ecomm">    <i class="bx bxs-cart-add"></i>Add to Cart</a>
-                    <a href="" class="btn btn-light btn-ecomm" data-bs-toggle="modal" data-bs-target="#QuickViewProduct"><i class="bx bx-zoom-in"></i>Quick View</a>
-                </div>
-            </div>
-       </div>`
+        </div>
+        `
     ).join('')}
     `;
 };
