3cae11080c1b9c36531af4dd61053a518b53343d
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-07 19:15:09 -0600

    inntento de mostrar productos de tienda => 4435

--- js/shop-filter.js
+++ js/shop-filter.js
@@ -11,22 +11,9 @@ search.addWidgets([
         container: '#container-hits-demo'
     }),
 
-    instantsearch.widgets.index({indexName: 'menu-products-demo', indexId: '4434'}).addWidgets([
+    instantsearch.widgets.index({indexName: 'menu-products-demo', indexId: '4435'}).addWidgets([
         instantsearch.widgets.hits({
             container: '#container-hits',
-            templates: {
-                item: `
-                <div>
-                    <div class="hit-name">
-                        {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
-                    </div>
-                    <div class="hit-description">
-                        {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
-                    </div>
-                </div>
-                
-                `
-            }
         })
     ])
 ]);
