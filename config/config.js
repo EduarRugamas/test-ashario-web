ebe18a39d0e4e9b02204821b694a6edeedc38274
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-08 15:14:53 -0600

    cambiando credenciales a production

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
-export const searchClient = algoliasearch('VFM4X0N23A', 'd16d4804f78c38777368ea8ced79e56e');
+export const searchClient = algoliasearch('VFM4X0N23A', '621cb93dc0fd5c6442f9ce2f1d82e44f');
 
 
