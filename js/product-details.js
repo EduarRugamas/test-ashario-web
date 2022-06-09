f29a74bbede81f350a3c9f56b0850abd1d40ab59
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-08 18:21:27 -0600

    agregando intento de solucion de bug

--- js/product-details.js
+++ js/product-details.js
@@ -10,8 +10,9 @@ console.log('el objecto id', objectID);
 
 index.search(objectID).then( ( {hits} ) => {
     // usando el hits[0].name etc
-    document.getElementById('product-details').innerHTML = `
-    
+    const contenedor = document.getElementById('product-details');
+
+    contenedor.innerHTML = `
     <!--start breadcrumb-->
        <section class="py-3 border-bottom border-top d-none d-md-flex bg-light">
             <div class="container">
