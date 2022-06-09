5b7dd0593a4705b61abc1ea4d2ff90e171475526
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-08 18:40:40 -0600

    agregando intento de solucion de bug 7

--- js/shop-filter.js
+++ js/shop-filter.js
@@ -6,6 +6,15 @@ const search = instantsearch({
     routing: true
 });
 
+
+search.use(
+    instantsearch.middlewares.createInsightsMiddleware({
+        insightsClient: aa
+    })
+);
+
+aa('setUserToken', 'user-1')
+
 function QuickViewModal (items) {
 
     console.log(items.name);
@@ -23,8 +32,6 @@ function QuickViewModal (items) {
 
 const HitsRender = (renderOptions, isFirstRender) => {
     const { hits, results, sendEvent, widgetParams } = renderOptions;
-
-    const url = '' + hits.objectID;
 
     widgetParams.container.innerHTML = `
             ${hits.map( item =>
@@ -56,7 +63,7 @@ const HitsRender = (renderOptions, isFirstRender) => {
                         </div>
                         <div class="product-action mt-2">
                             <div class="d-grid gap-2">
-                                <a href="product-details.html?objectID=${item.objectID}" class="btn btn-dark btn-ecomm">Product Details</a>
+                                <a href="product-details.html?objectID=${item.objectID}" ${sendEvent('click', hits, 'search Result clicked')} class="btn btn-dark btn-ecomm">Product Details</a>
                                 <a href="" class="btn btn-light btn-ecomm" data-bs-toggle="modal" data-bs-target="#QuickViewProduct" id="QuickViewModalBtn" onclick="QuickViewModal(item)"><i class="bx bx-zoom-in"></i>Quick View</a>
                             </div>
                         </div>
