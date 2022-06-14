0e26db873918ec7c9580a1243cd4e7eb4f6d8b7c
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-14 09:10:21 -0600

    intento de resolver bug de product-details 2

--- js/product-details.js
+++ js/product-details.js
@@ -1,15 +1,15 @@
-import { searchClient } from '../config/config.js'
+import { searchClient } from '../config/config.js';
 const urlParams = new URLSearchParams(window.location.search);
-const objectID = urlParams.get('objectID');
+const product_id = urlParams.get('product_id');
 
 const indexName = 'menu-products-demo';
 const client = algoliasearch('VFM4X0N23A', 'd16d4804f78c38777368ea8ced79e56e');
 const index = client.initIndex(indexName);
 
-console.log('el objecto id', objectID);
+console.log('el objecto id', product_id);
 
 index.search('query', {
-    filters: `objectID:${objectID}`
+    filters: `product_id:${product_id}`
 }).then( ({hits})=> {
     console.log(hits);
 });
