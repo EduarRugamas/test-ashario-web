31d78faad92edbae9bd988fce987ddea03ec7623
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-22 09:35:15 -0600

    intento de hacer un link a toda la tarjeta de los producto

--- js/stores.js
+++ js/stores.js
@@ -11,42 +11,46 @@ const search = instantsearch({
 const HitsRender = (renderOptions, isFirstRender) => {
     const { hits, results, BindEvent, widgetParams } = renderOptions;
 
+    console.log(hits);
+
     widgetParams.container.innerHTML = `
             ${hits.map( item =>
         `
         <div class="col">
             <div class="card rounded-0 product-card">
-                <img src="${item.image_urls}" class="card-img-top" alt="..." id="imagen-product">
-                <div class="card-body">
-                    <div class="product-info">
-                        <a href="javascript:;">
-                            <p class="product-catergory font-13 mb-1">${item.brand}</p>
-                            <p class="product-catergory font-13 mb-1">${item.brand_subtype}</p>
-                            <p class="product-catergory font-13 mb-1">${item.objectID}</p>
-                        </a>
-                        <a href="javascript:;">
-                            <h6 class="product-name mb-2">${instantsearch.highlight({ attribute: 'name', hit: item })}</h6>
-                        </a>
-                        <div class="d-flex align-items-center">
-                            <div class="mb-1 product-price">
-                                <span class="fs-5">\$ ${item.bucket_price}</span>
+                <img src="${item.image_urls}" class="card-img-top" alt="${item.name}" id="imagen-product">
+                <a href="product-details.html?objectID=${item.objectID}">
+                    <div class="card-body">
+                        <div class="product-info">
+                            <a href="javascript:;">
+                                <p class="product-catergory font-13 mb-1">${item.brand}</p>
+                                <p class="product-catergory font-13 mb-1">${item.brand_subtype}</p>
+                            <!--- <p class="product-catergory font-13 mb-1">${item.objectID}</p> --->
+                            </a>
+                            <a href="javascript:;">
+                                <h6 class="product-name mb-2">${instantsearch.highlight({ attribute: 'name', hit: item })}</h6>
+                            </a>
+                            <div class="d-flex align-items-center">
+                                <div class="mb-1 product-price">
+                                    <span class="fs-5">\$ ${item.bucket_price}</span>
+                                </div>
+                                <div class="cursor-pointer ms-auto">
+                                    <i class="bx bxs-star text-warning"></i>
+                                    <i class="bx bxs-star text-warning"></i>
+                                    <i class="bx bxs-star text-warning"></i>
+                                    <i class="bx bxs-star text-warning"></i>
+                                    <i class="bx bxs-star text-warning"></i>
+                                </div>
                             </div>
-                            <div class="cursor-pointer ms-auto">
-                                <i class="bx bxs-star text-warning"></i>
-                                <i class="bx bxs-star text-warning"></i>
-                                <i class="bx bxs-star text-warning"></i>
-                                <i class="bx bxs-star text-warning"></i>
-                                <i class="bx bxs-star text-warning"></i>
-                            </div>
-                        </div>
-                        <div class="product-action mt-2">
-                            <div class="d-grid gap-2">
-                                <a href="product-details.html?objectID=${item.objectID}" class="btn btn-dark btn-ecomm">Product Details</a>
-                                <a href="" class="btn btn-light btn-ecomm" data-bs-toggle="modal" data-bs-target="#QuickViewProduct" id="QuickViewModalBtn"><i class="bx bx-zoom-in"></i>Quick View</a>
+                            <div class="product-action mt-2">
+                                <div class="d-grid gap-2">
+                                    <a href="product-details.html?objectID=${item.objectID}" class="btn btn-dark btn-ecomm">Product Details</a>
+                                    <a href="" class="btn btn-light btn-ecomm" data-bs-toggle="modal" data-bs-target="#QuickViewProduct" id="QuickViewModalBtn"><i class="bx bx-zoom-in"></i>Quick View</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>  
             </div>
         </div>
         `
