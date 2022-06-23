b8bf8602d11bd525fec94cea1d0179234c9e3c91
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-23 09:49:42 -0600

    agregando funcionalidad de carusel slider -> 16

--- js/product-details.js
+++ js/product-details.js
@@ -35,7 +35,7 @@ index.search('', {
                                 <div class="image-zoom-section">
                                     <div class="product-gallery owl-carousel owl-theme border mb-3 p-3 owl-loaded owl-drag">
                                         <div class="owl-stage-outer">
-                                            <div class="owl-item" style="width: 506px; margin-right: 10px;">
+                                            <div class="owl-item" style="width: 400px;">
                                                 <div class="item">
                                                     <img src="" alt="" class="img-fluid" id="imagen_carusel" style="background-size: cover; object-fit: cover; object-position: center;">
                                                 </div>
