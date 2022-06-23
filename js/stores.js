2e8fb05690a0c89b1db00f70e96959f4060ebd53
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-23 11:57:31 -0600

    agregando funcionalidad de carusel slider -> 29

--- js/stores.js
+++ js/stores.js
@@ -20,7 +20,7 @@ const HitsRender = (renderOptions, isFirstRender) => {
         <div class="col">
             <div class="card rounded-0 product-card">
                         <a href="product-details.html?objectID=${item.objectID}">
-                            <img src="${item.image_urls === 0 ? '../assets/images/errors-images/image-not-found.jpeg' : item.image_urls }" class="card-img-top" alt="${item.name}" id="imagen-product">
+                            <img src="${ item.image_urls === 0 ? item.image_urls : '../assets/images/errors-images/image-not-found.jpeg' }" class="card-img-top" alt="${item.name}" id="imagen-product">
                         </a>
                     <div class="card-body">
                         <div class="product-info">
