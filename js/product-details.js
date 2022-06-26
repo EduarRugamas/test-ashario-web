6cd380c61a8913c9fabf7cb8e965f117931f416c
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-26 10:26:02 -0600

    agregando cambios a valores dinamicos -> 24

--- js/product-details.js
+++ js/product-details.js
@@ -192,13 +192,7 @@ index.search('', {
             text_price.id = "h4_price";
             text_price.textContent = `$ ${hits[0].price_ounce}`;
             container_price.appendChild(text_price);
-        }
-    //|| (hist[0].available_weights.length === 1 && hits[0].available_weights  === "half gram")
-        if (option_weigh_current === "half gram") {
-            //  let container_price = document.getElementById('text_price')
-            //  console.log('esta aqui',option_weigh_current);
-            //  container_price.innerHTML=`<h4 class="mb-0" id="h4_price">$ ${hits[0].price_half_gram}</h4>`;
-            //  console.log('paso a aqui ')
+        }else if (option_weigh_current === "half gram") {
             let text_price = document.createElement('h4');
             text_price.className = "mb-0";
             text_price.id = "h4_price";
