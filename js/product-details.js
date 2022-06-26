3273e2c9cc262c2d295bce8aef1d28d5263a660a
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-26 10:04:22 -0600

    agregando cambios a valores dinamicos -> 19

--- js/product-details.js
+++ js/product-details.js
@@ -195,11 +195,9 @@ index.search('', {
         }
 
         if (option_weigh_current === 'half gram') {
+            let container_price = document.getElementById('text_price').
             console.log('esta aqui en el half gram');
-            let text_price = document.createElement('h4');
-            text_price.className = "mb-0";
-            text_price.id = "h4_price";
-            text_price.textContent = `$ ${hits[0].price_half_gram}`;
+             container_price.innerHTML=`<h4 class="mb-0" id="h4_price">$ ${hits[0].price_half_gram}</h4>`;
         }
     }
 
