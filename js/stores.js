ad309c0c783b94a91d63219e93fd8f921ac735ad
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-27 01:14:53 -0600

    filtrando por cateria flowers --> 13

--- js/stores.js
+++ js/stores.js
@@ -60,8 +60,8 @@ const CustomHits = instantsearch.connectors.connectHits(HitsRender);
 //   fin de widgets custom o personalizados
 
 
-    index.search('flowers', {
-        filters: 'brand_subtype:flowers'
+    index.search('', {
+        filters: 'kind:flower'
     }).then( ({hits}) => {
         console.log(hits);
         search.addWidgets([
