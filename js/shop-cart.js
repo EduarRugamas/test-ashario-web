9c9ad481f5c492653237bc94308945c6bdbfc70e
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-28 10:44:49 -0600

    agregando funcionalidad de carrito --> 26

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -1,14 +1,17 @@
 import { searchClient } from '../config/config.js';
 import { data } from '../config/data.js';
 
-window.addEventListener("message", receiveMessage, false);
+let frame = document.getElementById("jane-menu");
+frame.contentWindow.postMessage(data, "*");
 
-function receiveMessage(event) {
-    let payload = event.data && event.data.payload;
-    let messageType = event.data && event.data.messageType;
-
-    if (messageType === "loadingEvent" && payload.name === "headlessAppLoaded") {
-        let frame = document.getElementById("jane-menu");
-        frame.contentWindow.postMessage(data, "*");
-    }
-}
+// window.addEventListener("message", receiveMessage, false);
+//
+// function receiveMessage(event) {
+//     let payload = event.data && event.data.payload;
+//     let messageType = event.data && event.data.messageType;
+//
+//     if (messageType === "loadingEvent" && payload.name === "headlessAppLoaded") {
+//         let frame = document.getElementById("jane-menu");
+//         frame.contentWindow.postMessage(data, "*");
+//     }
+// }
