66b6359c03f85b9d21c88f8b32520c1a2300bfad
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-28 00:44:32 -0600

    agregando funcionalidad de carrito --> 19

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -1,12 +1,44 @@
 import { searchClient } from '../config/config.js';
 
-let data = {
-    data: {
-        messageType: "loadingEvent",
-        payload: {
-            name: "headlessAppLoaded"
-        }
-    }
+data = {
+    messageType: 'buildCart',
+    payload: {
+        products: [],
+        user: {
+            firstName: 'John',
+            lastName: 'Smith',
+            birthDate: '1988-04-20',
+            phone: '5551112222',
+            email: 'johnsmith@gmail.com',
+            externalId: '12345',
+        },
+        cognitoToken: undefined,
+        storeId: 68,
+        headlessPartnerName: 'Partner Name',
+        headlessCheckoutPartnerId: '1234-asdf-5678-ghjk', // provided by Jane
+        options: {
+            theme: {
+                themeColor: "#004f89",
+                navigationColor: "#ffc03c",
+                ctaTextColor: "#ffffff",
+                backgroundColor: '#f3e9db',
+                font: {
+                    fontFamily: 'Gilroy',
+                    url: '//db.onlinewebfonts.com/t/1dc8ecd8056a5ea7aa7de1db42b5b639.woff2',
+                    color: '#111C4E'
+                },
+                calloutBackground: '#FFC03C',
+                calloutTextColor: '#111C4E',
+                buttonBorderRadius: '24px',
+            },
+            redirectUrl: 'https://harmonious-mooncake-eb8276.netlify.app/views/shop-grid-left-sidebar.html',
+            disableWeightSelection: false,// setting this to true will disable changing weight selections
+            disableLoadingSpinner: false,
+            disableAuthFeatures: true, //setting this to false will allow users to sign in but only in the checkout iFrame
+            disablePrepayment: false,
+            kiosk: false // set to true to enable kiosk mode
+        },
+    },
 }
 
 window.addEventListener("message", receiveMessage, false);
