050b27682772566023217030bd8c2db55f168cc7
EduarRugamas <juaneduardo021299@hotmail.com>
2022-06-27 21:59:54 -0600

    creando carousel en items de productos --> 8

--- js/product-details.js
+++ js/product-details.js
@@ -360,7 +360,7 @@ const images = hits[0].image_urls;
 
 let posicionActual = 0;
 let $container_img = document.querySelector('#imagen_carusel');
-const TIEMPO_INTERVALO_MILESIMAS_SEG = 15000;
+const TIEMPO_INTERVALO_MILESIMAS_SEG = 9000;
 let intervalo;
 
 if (images.length === 0) {
